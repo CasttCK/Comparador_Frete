0f8e42097253f42bccbad1b53f582f38d925118e
CasttCK <vini.kronemberger@gmail.com>
2023-05-16 09:38:59 -0300

    Feat: Criação do projeto

---
+++ Comparador_Frete/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comparador_Frete.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
